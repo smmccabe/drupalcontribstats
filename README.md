e3778ca1c31f00afbe5a006fb97e843abd5a5083
Shawn McCabe <smccabe@acromediainc.com>
2019-10-16 14:00:01 -0700

    Make more generic and add a bunch of command line options

--- README.md
+++ README.md
@@ -5,5 +5,23 @@ Simple go app for pulling and scanning through drupal modules and totalling up t
 ## Usage
 
 ```
-go run main.go
+drupalcontribstats commerce
+
+# You can pass in as many projects as you want
+drupalcontribstats commerce commerce_pos
+
+# by default it takes the last year, but you can pass in custom starts and/or ends
+drupalcontribstats --since 20190101 --until 20200101 commerce commerce_pos
+
+# a custom cache directory, you can always point this where you have many or all of the project cloned already
+drupalcontribstats --cacheDir /home/me/dev drupal paragraphs
+
+# pass in a list as a text file instead
+drupalcontribstats --list mycustomlist.txt
+
+# Will process the custom list plus the ones provided by the command line
+drupalcontribstats --list mycustomlist.txt paragraphs
+
+# Outputs the names of all the projects scanned and the contributors with their totals
+drupalcontribstats commerce commerce_pos --verbose
 ```
--- main.go
+++ main.go
@@ -1,52 +1,99 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
+	"log"
 	"os"
+	"path"
 	"regexp"
 	"strings"
 	"time"
-	"bufio"
-	"log"
 
 	git "gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing/object"
 
-	"github.com/davecgh/go-spew/spew"
+	"github.com/btubbs/datetime"
 )
 
 func main() {
 	var contributors = make(map[string]int)
 
-	tallyRepo("https://git.drupalcode.org/project/commerce.git", contributors)
+	sinceFlag := flag.String("since", "now - 1 year", "a to start from in any ISO 8601 format")
+	untilFlag := flag.String("until", "now", "a to count until in any ISO 8601 format")
+	cacheDir := flag.String("cacheDir", "/tmp/drupalcontribstats", "folder to store repositories, can already be populated")
+	listFile := flag.String("list", "", "a file with a newline seperated list of drupal project to scan")
+	verbose := flag.Bool("verbose", false, "Display extra debug information")
 
-	file, err := os.Open("list.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
+	flag.Parse()
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		text := scanner.Text()
-		fmt.Println(text)
-		tallyRepo("https://git.drupalcode.org/project/commerce_" + text + ".git", contributors)
+	var err error
+	var since time.Time
+	if *sinceFlag == "now - 1 year" {
+		since = time.Now().AddDate(-1, 0, 0)
+	} else {
+		since, err = datetime.Parse(*sinceFlag, time.UTC)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
-	
-	spew.Dump(contributors)
+
+	var until time.Time
+	if *untilFlag == "now" {
+		until = time.Now()
+	} else {
+		until, err = datetime.Parse(*untilFlag, time.UTC)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	if *listFile != "" {
+		file, err := os.Open(*listFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			text := scanner.Text()
+			tallyRepo(text, contributors, since, until, *cacheDir, *verbose)
+		}
+	}
+
+	args := flag.Args()
+	if len(args) > 0 {
+		for _, arg := range args {
+			tallyRepo(arg, contributors, since, until, *cacheDir, *verbose)
+		}
+	}
+
+	if *verbose {
+		for contributor, contributions := range contributors {
+			fmt.Println(contributor+":", contributions)
+		}
+	}
 	fmt.Println(len(contributors))
 }
 
-func tallyRepo(url string, contributors map[string]int) {
+func tallyRepo(project string, contributors map[string]int, since time.Time, until time.Time, cacheDir string, verbose bool) {
 	re := regexp.MustCompile(`Issue #(.*) by ([^:]*)`)
-	folder := url[strings.LastIndex(url, "/")+1:]
-	path := "/tmp/git/" + folder
+	url := "https://git.drupalcode.org/project/" + project + ".git"
+	path := path.Join(cacheDir, project)
 
 	var r *git.Repository
 	var err error
 	if _, err := os.Stat(path); !os.IsNotExist(err) {
+		if verbose {
+			fmt.Println("Opening existing " + project + "...")
+		}
 		r, err = git.PlainOpen(path)
 	} else {
+		if verbose {
+			fmt.Println("Cloning " + project + "...")
+		}
 		r, err = git.PlainClone(path, false, &git.CloneOptions{
 			URL: url,
 		})
@@ -58,8 +105,11 @@ func tallyRepo(url string, contributors map[string]int) {
 
 	ref, err := r.Head()
 
-	since := time.Date(2018, time.October, 8, 0, 0, 0, 0, time.UTC)
-	cIter, err := r.Log(&git.LogOptions{From: ref.Hash(), Since: &since})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cIter, err := r.Log(&git.LogOptions{From: ref.Hash(), Since: &since, Until: &until})
 
 	if err != nil {
 		log.Fatal(err)
